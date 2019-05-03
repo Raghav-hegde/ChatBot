971c7017a43f6402d9164591aced662e0d38ae99
Raghavendra Hegde <hegderaghav66@gmail.com>
2019-05-03 23:05:29 +0530

    Removed unwanted methods

--- index.js
+++ index.js
@@ -13,47 +13,7 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/echo", function(req, res) {
-    var speech =
-        req.body.queryResult &&
-        req.body.queryResult.parameters &&
-        req.body.queryResult.parameters.echoText ?
-        req.body.queryResult.parameters.echoText :
-        "Seems like some problem. Speak again.";
-    return res.json({
-        fulfillmentText: speech,
-        source: "webhook-echo-sample"
-    });
-});
-
-restService.post("/calculator", function(req, res) {
-    var num1 = req.body.queryResult.parameters.number1;
-    var num2 = req.body.queryResult.parameters.number2;
-    var operation = req.body.queryResult.parameters.operator;
-    var result = 0;
-    var answer = "";
-    switch (operation) {
-        case "Add":
-            result = num1 + num2;
-            answer = num1 + " + " + num2 + " = " + result;
-            break;
-        case "Subtract":
-            result = num1 - num2;
-            answer = num1 + " - " + num2 + " = " + result;
-            break;
-        case "Multiply":
-            result = num1 * num2;
-            answer = num1 + " * " + num2 + " = " + result;
-            break;
-            defaut: answer = "I'm facing difficulty in that. Talk to the developer";
-    }
-    return res.json({
-        fulfillmentText: answer,
-        source: "webhook-addition-sample"
-    });
-});
-
-restService.post("/poc", function(req, res) {
+restService.post("/getSOStatus", function(req, res) {
     //console.log('event data: ' + JSON.stringify(event.data));	
     //var replyMsg;
     //var intent = req.data['intent'];
